c331165ea5bd38a4735ac91a88812e931d914735
Tom May <73091215+tom-may@users.noreply.github.com>
2021-02-16 10:43:44 +1300

    list, add, update and delete functions added to API

--- client/apis/tasks.js
+++ client/apis/tasks.js
@@ -1,9 +1,43 @@
 import request from 'superagent'
 
-const rootUrl = '/api/v1'
+const rootUrl = '/api/v1/tasks'
 
 export function getTodos () {
   return request
-    .get(rootUrl + '/tasks')
+    .get(rootUrl)
     .then(res => {return res.body})
+    .catch(errorHandler('GET', '/v1/tasks'))
+  }
+
+  export function addTodo (todo) {
+    return request
+      .post(rootUrl)
+      .send(todo)
+      .then(res => {return res.body})
+      .catch(errorHandler('POST', '/v1/tasks'))
+  }
+
+  export function updateTodo (todo) {
+    return request
+      .patch(`${rootUrl}/${todo.id}`)
+      .send(todo)
+      .then(res => {return res.body})
+      .catch(errorHandler('PATCH', '/v1/tasks'))
+  }
+
+  export function deleteTodo (todoId) {
+    return request
+      .del(`${rootUrl}/${todoId}`)
+      .then(res => res)
+      .catch(errorHandler('DELETE', '/v1/tasks'))
+  }
+
+  function errorHandler (method, route) {
+    return (err) => {
+      if (err.message === 'Not Found') {
+        throw Error(`Error: You need to implement an API route for ${method} ${route}`)
+      } else {
+        throw Error(`${err.message} on ${method} ${route}`)
+      }
+    }
   }
--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -16,4 +16,6 @@ router.get('/', (req, res) => {
     })
 })
 
+
+
 module.exports = router
