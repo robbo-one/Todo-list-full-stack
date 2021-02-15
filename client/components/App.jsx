52862be2c0ebcc4874a91c4b24f9c4ff201b758f
Lara Hayward <22077219+haywardlara@users.noreply.github.com>
2021-02-16 11:22:40 +1300

    toggle complete all tasks

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -15,9 +15,6 @@ function App (props) {
         <AddTodo />
       </header>
       <section className="main">
-        {props.tasks.length > 0 && (
-        <div><input id="toggle-all" className="toggle-all" type="checkbox" />
-        <label htmlFor="toggle-all">Mark all as complete</label></div>)}
         <Tasks />
       </section>
       <footer className="footer"></footer>
--- client/components/Tasks.jsx
+++ client/components/Tasks.jsx
@@ -8,6 +8,8 @@ import { deleteTask, taskCompleteToggle, updateTask } from "../actions"
 function Tasks(props) {
   const t = props.tasks
 
+  const [allComplete, setAllComplete] = useState(false)
+
   const deleteClickHandler = (id) => {
     props.dispatch(deleteTask(id))
   }
@@ -18,25 +20,51 @@ function Tasks(props) {
 
   const keyDownHandler = (evt, id) => {
     if (evt.keyCode == 13) {
-      // console.log(evt.target.value)
+      //enter
       props.dispatch(updateTask(id, evt.target.value))
       evt.target.value = ""
       evt.target.parentNode.className = ""
     }
     if (evt.keyCode == 27) {
+      //escape
       evt.target.value = ""
       evt.target.parentNode.className = ""
     }
   }
 
-  const toggleClickHander = (id, completedStatus) => {
+  const toggleClickHandler = (id, completedStatus) => {
     props.dispatch(taskCompleteToggle(id, completedStatus))
+  }
+
+  const toggleAllClickHandler = () => {
+    if(allComplete){
+      t.map((task) => {
+        return props.dispatch(taskCompleteToggle(task.id, "yes"))
+      })
+    } else {
+      t.map((task) => {
+        return props.dispatch(taskCompleteToggle(task.id, "no"))
+      })
+    }
+    setAllComplete(!allComplete)
   }
 
   return (
     <>
+      {t.length > 0 && ( //check if any tasks exist
+        <div>
+          <input
+            id="toggle-all"
+            className="toggle-all"
+            type="checkbox"
+            onClick={toggleAllClickHandler}
+          />
+          <label htmlFor="toggle-all">Mark all as complete</label>
+        </div>
+      )}
       <ul className="todo-list">
         {t.map((task) => {
+          //show tasks
           return (
             <li
               key={task.id}
@@ -46,18 +74,19 @@ function Tasks(props) {
                 <input
                   className="toggle"
                   type="checkbox"
-                  onClick={() => toggleClickHander(task.id, task.completed)}
+                  onClick={() => toggleClickHandler(task.id, task.completed)} //toggle complete
+                  checked={task.completed == "yes"}
                 />
                 <label onDoubleClick={doubleClickHandler}>{task.task}</label>
                 <button
                   className="destroy"
-                  onClick={() => deleteClickHandler(task.id)}
+                  onClick={() => deleteClickHandler(task.id)} //delete task
                 ></button>
               </div>
               <input
                 className="edit"
                 placeholder={task.task}
-                onKeyDown={(evt) => keyDownHandler(evt, task.id)}
+                onKeyDown={(evt) => keyDownHandler(evt, task.id)} //edit task
               />
             </li>
           )
