5cbe249be07bf293c8184804a4f8299f9eab3b13
Julia Kurganskaia <17925683+julia-kurganskaia@users.noreply.github.com>
2021-02-16 11:55:10 +1300

    Display tasks with CSS< but need to fix

--- client/components/AddTodo.jsx
+++ client/components/AddTodo.jsx
@@ -12,14 +12,59 @@ function AddTodo (props) {
   }, []);
 console.log(props)
   return (
-    <>
+    // <>
+    //   <input className="new-todo" placeholder="What needs to be done?" autoFocus={true} />
+    //   <div>
+    //     {props.todos.map(todo => (
+    //       <div key={todo.id}>{todo.todoTask}</div>
+    //     ))}
+    //   </div>
+    // </>
+    <div>
       <input className="new-todo" placeholder="What needs to be done?" autoFocus={true} />
-      <div>
-        {props.todos.map(todo => (
-          <div key={todo.id}>{todo.todoTask}</div>
-        ))}
-      </div>
-    </>
+      <section className="main">
+      <input id="toggle-all" className="toggle-all" type="checkbox" />
+      <label htmlFor="toggle-all">Mark all as complete</label>
+      <ul className="todo-list">
+        {/* <!-- These are here just to show the structure of the list items -->
+        <!-- List items should get the class `editing` when editing and `completed` when marked as completed --> */}
+        <li>
+          {props.todos.map(todo => (
+          <div className="view">
+            <input className="toggle" type="checkbox" />
+            
+          <label key={todo.id}>{todo.todoTask}</label>
+        
+            
+            
+          </div>))}
+          <input className="edit" value="Create a TodoMVC template" />
+        </li>
+
+
+        {/* {props.todos.map(todo => (
+          <li key={todo.id}>{todo.todoTask}</li>
+        ))} */}
+
+        {/* <li className="completed">
+          <div className="view">
+            <input className="toggle" type="checkbox" checked />
+            <label>Taste JavaScript</label>
+            <button className="destroy"></button>
+          </div>
+          <input className="edit" value="Create a TodoMVC template" />
+        </li>
+        <li>
+          <div className="view">
+            <input className="toggle" type="checkbox" />
+            <label>Buy a unicorn</label>
+            <button className="destroy"></button>
+          </div>
+          <input className="edit" value="Rule the web" />
+        </li> */}
+      </ul>
+      </section>
+    </div>
   )
 }
 
