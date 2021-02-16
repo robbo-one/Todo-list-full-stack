4fe198989c2dacf5208df5bae0195c438de199bb
Lara Hayward <22077219+haywardlara@users.noreply.github.com>
2021-02-16 16:27:30 +1300

    change selected filter button, i hate how hard it was wth

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -1,21 +1,34 @@
-import React from "react"
+import React, { useState } from "react"
 import { connect } from "react-redux"
-import { deleteTask } from '../actions'
+import { deleteTask } from "../actions"
 
 function Footer(props) {
   const t = props.tasks
+
+  // const [active, setActive] = useState(['selected', '', ''])
+  const [selected, setSelected] = useState("All")
 
   const checkIfCompletedItemExists = () => {
     return t.find((task) => task.completed == "yes")
   }
 
   const clearCompleted = () => {
-    t.map(task => {
-      if(task.completed == 'yes'){
+    t.map((task) => {
+      if (task.completed == "yes") {
         return props.dispatch(deleteTask(task.id))
       }
     })
   }
+
+  // const handleSelect = (element) => {
+  //   if(element == 1){
+  //     setActive(['selected', '', ''])
+  //   } else if(element == 2){
+  //     setActive(['', 'selected', ''])
+  //   } else if(element == 3) {
+  //     setActive(['', '', 'selected'])
+  //   }
+  // }
 
   return (
     <>
@@ -27,19 +40,37 @@ function Footer(props) {
           {/* <!-- Remove this if you don't implement routing --> */}
           <ul className="filters">
             <li>
-              <a className="selected" href="#/">
+              <a
+                className={selected == "All" ? "selected" : ""}
+                href="#/"
+                onClick={() => setSelected("All")}
+              >
                 All
               </a>
             </li>
             <li>
-              <a href="#/active">Active</a>
+              <a
+                className={selected == "Active" ? "selected" : ""}
+                href="#/active"
+                onClick={() => setSelected("Active")}
+              >
+                Active
+              </a>
             </li>
             <li>
-              <a href="#/completed">Completed</a>
+              <a
+                className={selected == "Completed" ? "selected" : ""}
+                href="#/completed"
+                onClick={() => setSelected("Completed")}
+              >
+                Completed
+              </a>
             </li>
           </ul>
           {checkIfCompletedItemExists() && (
-            <button className="clear-completed" onClick={clearCompleted}>Clear completed</button>
+            <button className="clear-completed" onClick={clearCompleted}>
+              Clear completed
+            </button>
           )}
         </footer>
       )}
