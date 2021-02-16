5f3bc7c8a6c0f0f840e1fa0ab6d98a79732e8dc3
Michael Parkin <73506758+mike-parkin@users.noreply.github.com>
2021-02-17 10:38:21 +1300

    create env and index.js varibales

--- server/index.js
+++ server/index.js
@@ -1,4 +1,8 @@
+const path = require('path')
 const server = require('./server')
+
+const envPath = path.join(__dirname, '.env')
+require('dotenv').config({ path: envPath })
 
 const PORT = process.env.PORT || 3000
 
