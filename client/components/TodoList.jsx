82bfb5c6e7f03aaf2b38595ef997789776894ae1
Michael Parkin <73506758+mike-parkin@users.noreply.github.com>
2021-02-16 16:43:24 +1300

    refactored Todo component

--- client/components/TodoList.jsx
+++ client/components/TodoList.jsx
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 
-import { fetchTodos, removeTodo, changeTodo } from '../actions'
+import { fetchTodos } from '../actions'
+
+import Todo from './Todo'
 
 const TodoList = (props) => {
   const todos = props.todos
@@ -10,21 +12,6 @@ const TodoList = (props) => {
   useEffect(() => {
     props.dispatch(fetchTodos())
   }, [])
-
-  const handleDestroy = (id) => {
-    props.dispatch(removeTodo(id))
-      .then(() => {
-        props.dispatch(fetchTodos())
-      })
-  }
-
-  const handleCheckbox = (todo) => {
-    const newTodo = {...todo, completed: !todo.completed}
-    props.dispatch(changeTodo(newTodo))
-      .then(()=> {
-        props.dispatch(fetchTodos())
-      })
-  }
 
   const filterList = (filter, todos) => {
     switch (filter) {
@@ -39,28 +26,12 @@ const TodoList = (props) => {
     }
   }
 
-
   return (
     <ul className="todo-list">
           {filterList(filter, todos).map(todo => {
             return (
-              <li key={todo.id}
-                className={ todo.completed ? 'completed' : null }>
-                <div className="view">
-                  <input 
-                    className="toggle" 
-                    type="checkbox" 
-                    name="completed"
-                    defaultChecked={todo.completed}
-                    onChange={() => handleCheckbox(todo)}
-                  />
-                  <label>{todo.task}</label>
-                  <button 
-                    className="destroy"
-                    onClick={() => handleDestroy(todo.id)}>
-                  </button>
-                </div>  
-              </li>)
+              <Todo key={todo.id} todo={todo}/>
+            )            
           })}
     </ul>
   )
