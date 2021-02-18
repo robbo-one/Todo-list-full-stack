c353af5094557ac456272fa8bcf62fdcfe4672a9
student <student@eda-bc-10.localdomain>
2021-02-18 19:32:33 +1300

    Dispatch showing test data in global state

--- client/components/AllTodos.jsx
+++ client/components/AllTodos.jsx
@@ -1,43 +1,34 @@
 //render a list of all todos
 import React, { useEffect } from "react";
-import { connect } from 'react-redux'
-
-
+import { connect } from "react-redux";
 
 //render a list of todos, map through and show task field for all
 
-
 const AllTodos = (props) => {
-    useEffect(() => {}, [])
+  useEffect(() => {}, []);
 
-
-
-
-    return (
-        <>
-            <ul class="todo-list">
-
-            {props.todos.map(todo => (
-                <li key={todo.id}>
-                <div class="view">
-                <input class="toggle" type="checkbox" />
-                <label>{todo.task}</label>
-                <button class="destroy"></button>
-                </div>
-                <input class="edit" value="Buy a unicorn" />
-            </li>
-                ))}
-            </ul>
-        </>
-    )
-}
-
-
+  return (
+    <>
+      <ul class="todo-list">
+        {props.todos.map((todo) => (
+          <li key={todo.id}>
+            <div class="view">
+              <input class="toggle" type="checkbox" />
+              <label>{todo.task}</label>
+              <button class="destroy"></button>
+            </div>
+            <input class="edit" value="Buy a unicorn" />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
 const mapStateToProps = (globalState) => {
-    return {
-        todos: globalState.todos
-    }
-}
+  return {
+    todos: globalState.todos,
+  };
+};
 
-export default connect(mapStateToProps)(AllTodos)
+export default connect(mapStateToProps)(AllTodos);
