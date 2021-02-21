392bbe5ccb8243cf7fb24c5c36572d4d9fc1c950
naomi-porter <73915562+naomi-porter@users.noreply.github.com>
2021-02-22 09:55:57 +1300

    Created server/db/tasksExportFunctions,  server/routes/tasksExportRoutes, and updated info in server/server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const path = require('path')
 
+const tasksRoutes = require('./routes/tasksExportRoutes')
+
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join('server', 'public')))
+
+server.use('/api/v1/tasks', tasksRoutes)
 
 module.exports = server
