bcd9cb2709634e2d7454bee104c58b4b4b124746
Rob <robsc801@gmail.com>
2021-02-22 12:30:36 +1300

    started funcs

---
+++ client/apis/tasks.js
@@ -0,0 +1,34 @@
+import request, { del } from 'superagent'
+
+const rootUrl = '/api/v1'
+
+export function getTasks () {
+    return request.get(rootUrl + '/tasks')
+    .then(res => {
+        return res.body
+    })
+}
+
+export function addTask (task, priority) {
+    return request
+    .post(rootUrl + '/tasks')
+    .send({task : task,priority:priority})
+    .then(res => {
+         return res.body
+    })
+    .catch(err => {
+        console.log(err)
+        return res.send(400)
+     })
+    }
+
+    export function updateTask (id, newTask) {
+        return request
+        .patch(rootUrl)
+        .send({id :id, newTask : newTask})
+        .then(res => {
+            return res.send(200)
+
+        })
+    }
+
