11b9d94b709e02c2b8b60499eb91d6beb1617c6d
Rob <robsc801@gmail.com>
2021-02-25 10:23:51 +1300

    working on actions

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,15 +1,18 @@
 import {
-    getTasks
+    getTasks,
+    setTasks
 } from '../apis/tasks'   // imported from the API
 
 export const SET_TASKS = 'SET_TASKS'
 
+export const setTasks (tasks) {       // FIX WHAT"S GOING WRONG IN THIS FILE
+export const ADD_TASK = 'ADD_TASK'
 export function setTasks (tasks) {
 return {
     type: SET_TASKS,
     tasks
-}
-}
+   }
+ }
 
 export function fetchTasks () {
     return dispatch => {
